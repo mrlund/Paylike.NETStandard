3532558e702b3c269cfae912bf66e4ad9388b06d
Martin Lund <mrlund@hotmail.com>
2019-03-23 23:27:35 +0100

    added tests

---
+++ Paylike.NETStandard.Tests/ClientTests.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using Moq.Protected;
+using Newtonsoft.Json.Linq;
+using Paylike.NETStandard.Clients;
+using Paylike.NETStandard.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Paylike.NETStandard.Tests
+{
+    public class ClientTests
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClientTests()
+        {
+        }
+        [Fact]
+        public void CanInstatiateClient()
+        {
+            var client = new PaylikeClient(new HttpClient());
+            Assert.NotNull(client);
+        }
+
+        [Fact]
+        public async Task CanDeserializeTransactionResponse()
+        {
+            var client = new PaylikeClient(new HttpClient(new FakeHttpMessageHandler<Transaction>()));
+            var tx = await client.Transactions.Get("");
+            Assert.NotNull(tx);
+        }
+        [Fact]
+        public async Task CanDeserializeCardResponse()
+        {
+            var client = new PaylikeClient(new HttpClient(new FakeHttpMessageHandler<Card>()));
+            var tx = await client.Cards.Get("");
+            Assert.NotNull(tx);
+        }
+        [Fact]
+        public async Task CanDeserializePayoutResponse()
+        {
+            var client = new PaylikeClient(new HttpClient(new FakeHttpMessageHandler<Payout>()));
+            var tx = await client.Payouts.Create("",0,"DKK","","","");
+            Assert.NotNull(tx);
+        }
+        [Fact]
+        public async Task CanDeserializeErrorResponse()
+        {
+            var client = new PaylikeClient(new HttpClient(new FakeHttpMessageHandler<Payout>(true)));
+            var tx = await client.Payouts.Create("", 0, "DKK", "", "", "");
+            Assert.NotNull(tx);
+            Assert.False(tx.IsSuccessStatusCode);
+            Assert.False(string.IsNullOrEmpty(tx.ErrorMessage));
+        }
+    }
+
+}
---
+++ Paylike.NETStandard.Tests/FakeHttpMessageHandler.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using Paylike.NETStandard.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Paylike.NETStandard.Tests
+{
+    public class FakeHttpMessageHandler<T> : HttpMessageHandler
+    {
+        private bool _isError;
+        private object source;
+        public FakeHttpMessageHandler(bool isError = false)
+        {
+            _isError = isError;
+            if (isError)
+            {
+                source = JObject.FromObject(new ErrorResponse() { message = "Something went wrong!" });
+            }
+            else
+            {
+                source = new JObject();
+                ((JObject)source).Add(typeof(T).Name.ToLower(), JToken.FromObject(Activator.CreateInstance<T>()));
+            }
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var responseMessage = new HttpResponseMessage(_isError ? HttpStatusCode.BadRequest : HttpStatusCode.OK)
+            {
+                Content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(source))
+            };
+
+            return await Task.FromResult(responseMessage);
+        }
+    }
+}
